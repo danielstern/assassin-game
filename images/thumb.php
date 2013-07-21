2bcafb536d1d08df642f1589d1624716e55f3a54
daniel stern <daniel@danielstern.ca>
2013-07-21 10:04:56 -0400

    added a stupidly awesome thumb function

---
+++ images/thumb.php
@@ -0,0 +1,25 @@
+<?php
+// File and new size
+//$GET_['id'];
+ini_set("gd.jpeg_ignore_warning", 1);
+$filename = $_GET['id'] .'.png';
+$percent = 0.5;
+
+// Content type
+header('Content-Type: image/jpeg');
+
+// Get new sizes
+list($width, $height) = getimagesize($filename);
+$newwidth = $width * $percent;
+$newheight = $height * $percent;
+
+// Load
+$thumb = imagecreatetruecolor($newwidth, $newheight);
+$source = imagecreatefrompng($filename);
+
+// Resize
+imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+// Output
+imagejpeg($thumb);
+?>
